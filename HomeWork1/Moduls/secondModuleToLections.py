613ca3b80316021546d17139153e4a5b3e777155
Julia <hunter-777@yandex.ru>
2024-11-12 13:10:40 +0300

    hw4

--- HomeWork1/Moduls/secondModuleToLections.py
+++ HomeWork1/Moduls/secondModuleToLections.py
@@ -16,3 +16,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+from dis import dis
+
+def some_func():
+    a = "Я из второго модуля"
+    print("Я из второго модуля")
+    return a
+
+print(some_func())
+dis(some_func)
