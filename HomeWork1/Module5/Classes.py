e0a6540c9d2bc3a7dd14e727e7db95061445e4e1
Julia <hunter-777@yandex.ru>
2024-11-19 16:13:18 +0300

    Update Classes.py

--- HomeWork1/Module5/Classes.py
+++ HomeWork1/Module5/Classes.py
@@ -16,8 +16,12 @@ class Human:
     def birthday(self):
         self.age += 1
         print(f'У меня день рождения, мне теперь {self.age}')
-        if self.age >= 18:
+        if self.age >= 18 and self.age <= 25:
             print(f'Я достиг возраста, когда можно получить водительское удостоверение')
+        elif self.age < 18:
+            print(f'{self.name} еще не совершеннолетний')
+        else:
+            print(f'{self.name} совсем взрослый человек')
 
     def __del__(self):
         print(f'{self.name} ушёл')
@@ -37,20 +41,23 @@ class Human:
     def __str__(self):
         return f'{self.name} {self.surname}'
 
-den = Human("Denis", 0,15)
-max = Human("Maxim", 0,22)
+den = Human("Denis", 'Ivanov',15)
+max = Human("Maxim", 'Smirnov',22)
+sof = Human('Sofya', 'Petrova', 26)
 
 den.surname = "Popov"
 den.age = 17
 max.surname = "Petrov"
+
+
 #print(den.name, den.surname, den.age)
 #print(max.name, max.age)
-print(den)
-print(max)
+#print(den)
+#print(max)
 #print(max < den)
 #print(max > den)
 #print(max == den)
-print(den != max)
+#print(den != max)
 #max.birthday
 #del(max)
 #print(len(den))
